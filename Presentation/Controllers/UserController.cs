8b5ad8fd7c5ca75e0b20f6a025f4ad8999e645fa
Igres111 <sergikaralashvili@gmail.com>
2025-01-10 18:30:27 +0400

    Connect JWT token

---
+++ Presentation/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_commerce.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
