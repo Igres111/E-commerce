013a65d43a6464dffb14f6753acd7488864ebf68
Igres111 <sergikaralashvili@gmail.com>
2025-01-10 17:11:10 +0400

    Connect Db

---
+++ Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using E_commerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_commerce.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(entity => entity.Id);
+        }
+}
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using DotNetEnv;
+using E_commerce.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,9 +10,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+DotNetEnv.Env.Load();
+var connection = Environment.GetEnvironmentVariable("connection");
+var key = Environment.GetEnvironmentVariable("Key");
+if (string.IsNullOrEmpty(key))
+{
+    throw new Exception("JWT secret key is not set in the environment variables.");
+}
+builder.Services.AddDbContext<Context>(options =>
+{
+    options.UseSqlServer(connection);
+});
+
+
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
